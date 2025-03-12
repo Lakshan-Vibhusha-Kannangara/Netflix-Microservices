57f456affe4758d0a54091950bd4269a43fda24b
vibhushakannangara <vibhooshakannangara@gmail.com>
2025-03-12 22:44:04 +0530

    Initial Commit

---
+++ user-service/src/main/java/com/vibhusha/userservice/UserServerApplication.java
@@ -0,0 +1,13 @@
+package com.vibhusha.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class UserServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+}
