856a0df3a5922447f8cec17ec2a6994ee533a52d
vibhushakannangara <vibhooshakannangara@gmail.com>
2025-03-15 12:12:15 +0530

    add config server and service registry

---
+++ config-server/src/main/java/com/vibhusha/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.vibhusha.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableConfigServer
+@ComponentScan(basePackages = {"com.vibhusha"})
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
